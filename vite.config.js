ff533efd2525c1e5dcdf881f6980419e567f316d
yakzz <hwang90211@gmail.com>
2022-04-23 16:48:52 +0800

    測試1

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  base: './',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/vite_vanilla/' : './',
+  resolve: {
+    alias: {
+      '/@/': resolve(__dirname, './src/'),
+      '/@/assets/': resolve(__dirname, '/src/assets/'),
+      '/@/pages/': resolve(__dirname, '/src/pages/'),
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        topic: resolve(__dirname, 'topic.html'),
+        contact: resolve(__dirname, 'contact.html')
+      },
+      output: {
+        chunkFileNames: 'js/[name].[hash].js',
+        entryFileNames: 'js/[name].[hash].js',
+        assetFileNames: (info) => {
+          let type = info.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico|webp/i.test(type)) {
+            type = 'img';
+          }
+          return `${type}/[name]-[hash].[ext]`
+        },
+      }
+    },
+    cssCodeSplit: false
+  },
+  cssPreprocessOptions: {
+    scss: {
+      // additionalData: '@import "./styles/styles.scss";',
+    }
+  },
+  css: {
+    devSourceMap: true,
+  },
+})
